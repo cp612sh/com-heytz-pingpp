57f9b5c5c2eee9bb36ca9187c368371ef26ac2f7
mc <michaelchai@heytz.com>
2016-02-23 21:13:34 +0800

    update android libs

---
+++ www/pingpp.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /**
+   * @param {object|string} charge (JSON string or object)
+   * @param {Function} successCallback ['success']
+   * @param {Function} errorCallback ['fail'|'cancel'|'invalid']
+   */
+  createPayment: function (charge, successCallback, errorCallback) {
+    if (typeof charge === 'string') { JSON.parse(charge); }
+    if (charge.object === 'charge') {
+      cordova.exec(successCallback, errorCallback, "PingppPlugin", "createPayment", [JSON.stringify(charge)]);
+    } else {
+      return errorCallback('wrong charge object');
+    }
+  }
+};
